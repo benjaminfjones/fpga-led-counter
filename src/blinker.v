b9db2e24a627d7ed1f5e422da965a3183348442e
Benjamin Jones <benjaminfjones@gmail.com>
2014-10-07 16:06:34 -0700

    working simple blinker

--- src/blinker.v
+++ src/blinker.v
@@ -24,5 +24,24 @@ module blinker(
 	 output blink
     );
 
+// flipflop signals
+reg [24:0] counter_q, counter_d;
+
+// blink output is the MSB of counter_d, on half time, off half time
+assign blink = counter_d[24];
+
+// increment a the counter
+always@(counter_q) begin
+    counter_d = counter_q + 1'b1;
+end
+
+// implement the D-flipflop
+always@(posedge clk) begin
+   if (rst) begin
+	   counter_q <= 25'b0;
+	end else begin
+	    counter_q <= counter_d;
+	end
+end
 
 endmodule
