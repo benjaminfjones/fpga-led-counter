837053147c1421de53fae1cbe1ff1e6c878f7ab3
Benjamin Jones <benjaminfjones@gmail.com>
2014-10-08 16:34:49 -0700

    added a working test bench for pwm module

--- src/blinker.v
+++ src/blinker.v
@@ -27,18 +27,26 @@ module blinker(
 // flipflop signals
 reg [24:0] counter_q, counter_d;
 
+// direction bit, 0 is increasing, 1 is decreasing
+reg dir;
+
 // blink output is the MSB of counter_d, on half time, off half time
 assign blink = counter_d[24];
 
-// increment a the counter
+// increment or decrement the counter
 always@(counter_q) begin
-    counter_d = counter_q + 1'b1;
+    if (dir) begin
+	     counter_d = counter_q - 1'b1;
+    end else begin
+	     counter_d = counter_q + 1'b1;
+	 end
 end
 
 // implement the D-flipflop
 always@(posedge clk) begin
    if (rst) begin
 	   counter_q <= 25'b0;
+		dir <= 1'b0;
 	end else begin
 	    counter_q <= counter_d;
 	end
